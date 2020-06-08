6342353534f6a817d935e9cd2d6fa81bfe36879f
statm3n <statmanwisc@gmail.com>
2020-06-07 23:24:51 -0400

    started data cleaning and upload util functions

---
+++ utls/words.py
@@ -0,0 +1,65 @@
+import pyLDAvis
+import pyLDAvis.gensim
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import pandas as pd
+import numpy as np
+
+import nltk
+from nltk import FreqDist
+
+import gensim
+from gensim import corpora
+
+import re
+import spacy
+
+# function to plot most frequent terms
+def freq_words(x, terms = 30):
+    """
+    TODO: insert description
+    """
+    all_words = ' '.join([text for text in x])
+    all_words = all_words.split()
+
+    fdist = FreqDist(all_words)
+    words_df = pd.DataFrame({'word':list(fdist.keys()), 'count':list(fdist.values())})
+
+    # selecting top 20 most frequent words
+    d = words_df.nlargest(columns="count", n = terms) 
+    plt.figure(figsize=(20,5))
+    ax = sns.barplot(data=d, x= "word", y = "count")
+    ax.set(ylabel = 'Count')
+    plt.show()
+    
+
+def remove_stop_words(df, col='pros'):
+    """
+    TODO: insert description
+    """
+    
+    # remove unwanted characters, numbers and symbols
+    df_work = df.copy()
+    
+    df_work[col] = df_work[col].str.replace("[^a-zA-Z#]", " ")
+    
+    from nltk.corpus import stopwords
+    stop_words = stopwords.words('english')
+
+    # function to remove stopwords
+    def remove_stopwords(rev):
+        rev_new = " ".join([i for i in rev if i not in stop_words])
+        return rev_new
+
+    # remove short words (length < 3)
+    df_work[col] = df_work[col].apply(lambda x: ' '.join([w for w in x.split() if len(w)>2]))
+
+    # remove stopwords from the text
+    reviews = [remove_stopwords(r.split()) for r in df_work[col]]
+
+    # make entire text lowercase
+    reviews = [r.lower() for r in reviews]
+    
+    return reviews
+
